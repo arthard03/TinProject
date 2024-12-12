ca9248d0de6c61a1335e718b2d7b98cb2fe8f5dd
s28312 <s28312@pjwstk.edu.pl>
2024-12-12 20:01:16 +0100

    initial commit

---
+++ src/main/java/tin/tinproject/TinProjectApplication.java
@@ -0,0 +1,20 @@
+package tin.tinproject;
+
+import org.hibernate.engine.jdbc.connections.spi.JdbcConnectionAccess;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
+import org.springframework.context.annotation.Bean;
+
+
+
+@SpringBootApplication
+public class TinProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TinProjectApplication.class, args);
+
+
+	}
+	}
+
---
+++ src/test/java/tin/tinproject/TinProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tin.tinproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
